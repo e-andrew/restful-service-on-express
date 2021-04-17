60ddc4afbc0685b5895f23c31b10851a938abb23
e-andrew <66666556+e-andrew@users.noreply.github.com>
2021-04-17 22:15:53 +0300

    Creating src

--- README.md
+++
@@ -1,2 +0,0 @@
-# restful-service-on-express
-There are source files of RESTful services that are builded on Express.js.
---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const port = process.env.PORT || 3000
+const app = express();
+
+app.use(express.json());
+app.use(logger)
+
+const books = [
+    {id: 1, name: 'book1'},
+    {id: 2, name: 'book2'},
+    {id: 3, name: 'book3'}
+]
+
+
+app.get('/', logger, (req, res) => {
+    res.send("Hello World!");
+});
+
+app
+    .route('/api/books')
+    .get((req, res) => {
+    res.send(books);
+})
+    .post((req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        res.status(400).send("You should give book name")
+        return;
+    }
+    const book = {
+        id: books.length + 1,
+        name: req.body.name
+    };
+    books.push(book);
+    res.send(book);
+});
+
+app.get('/api/books/:id', (req, res) => {
+    const book = books.find(c => c.id === parseInt(req.params.id));
+    if (!book) res.status(404).send('The book with given id is not found');
+    res.send(book);
+});
+
+
+
+app
+    .route('/api/books/:id')
+    .put((req, res) => {
+    const book = books.find(c => c.id === parseInt(req.params.id));
+    if (!book) res.status(404).send('The book with given id is not found');
+    if (!req.body.name) res.status(400).send("You should give book name");
+    book.name = req.body.name;
+    res.send(book);
+})
+    .delete((req, res) => {
+    const book = books.find(c => c.id === parseInt(req.params.id));
+    if (!book) res.status(404).send('The book with given id is not found');
+
+    const index = books.indexOf(book);
+    books.splice(index, 1);
+    res.send(book);
+});
+
+
+function logger(req, res, next) {
+    console.log("Middleware request on express...");
+    next();
+};
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`))
